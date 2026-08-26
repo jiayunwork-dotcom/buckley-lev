f58bfc7cf4fa0a13182fde99a3198862e8b91d79
jiayun <jiayun_work@163.com>
2026-08-26 22:52:38 +0800

    init: task snapshot

---
+++ internal/welge/abortctx.go
@@ -0,0 +1,14 @@
+package welge
+
+import "context"
+
+func abortWelgeContext() error {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel()
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+		return nil
+	}
+}
